70b8e6c5133c9055fe86d46f51d39fad5da965f8
rinatryabikov <ttl2009@mail.ru>
2019-05-31 19:56:20 +0300

    change1

--- src/main/java/AccountService.java
+++ src/main/java/AccountService.java
@@ -3,6 +3,7 @@ public class AccountService {
     public static void main(String[] args) {
         System.out.println("Feature1");
         System.out.println("User1 finished feature1");
+        System.out.println("change1");
 
     }
 }
