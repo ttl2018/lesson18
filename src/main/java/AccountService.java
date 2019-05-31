997e0e01d459219f0a7d0f7fa7720c899eddc069
rinatryabikov <ttl2009@mail.ru>
2019-05-31 19:49:48 +0300

    Feature 1 is finished

--- src/main/java/AccountService.java
+++ src/main/java/AccountService.java
@@ -2,6 +2,7 @@ public class AccountService {
 
     public static void main(String[] args) {
         System.out.println("Feature1");
+        System.out.println("User1 finished feature1");
 
     }
 }
